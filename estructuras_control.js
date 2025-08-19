6d68cfa622106e21de0f35787d71d1d7a6d9d3fd
Santiago Salinas <salinassantiago2004@gmail.com>
2025-08-19 18:17:10 -0500

    vlidando estructura de control if else anidado

--- estructuras_control.js
+++ estructuras_control.js
@@ -8,25 +8,33 @@ let temperatura = 20;
   if (edad >= 18) 
     { console.log("Eres mayor de edad"); } // Resultado: "Eres mayor de edad"
 
+  // validando estructura de control if, con ejemplo de precio
   let dinero = 100;
    let precio = 50;
     if (dinero >= precio) 
         { console.log("Puedes comprar el producto"); } // Resultado: "Puedes comprar el producto"
 
+    // validando estructura de control if, con ejemplo de clave
     let password = "123456";
      if (password === "123456")
          { console.log("Acceso concedido"); } // Resultado: "Acceso concedido"
 
+     
+// validando estructura de control if, con ejemplo de edad
+// validando estructura de control else, con ejemplo de votar
      let edad2 = 12;
       if (edad2 >= 14) 
         { console.log("Puedes votar"); } 
       else { console.log("Aún no puedes votar"); } // Resultado: "Aún no puedes votar"
 
+      // validando estructura de control if, con ejemplo de numeros par he impar
+      // validando estructura de control else, con ejemplo de numeros par he impar
       let numero = 6; 
       if (numero % 2 === 0) 
         { console.log("Es par"); } 
       else { console.log("Es impar"); } // Resultado: "Es impar"
 
+      // validando estrucutura de control if else anidado
       let calificacion = 93; 
       if (calificacion >= 90) 
         { console.log("Excelente - A"); } 
